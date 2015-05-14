0b8305f2af5df3aa18cb7baa9a77945edb608d01
Matt Cameron <mcameron@avalanche.com.au>
2015-05-15 00:08:06 +1000

    Users can sign up to an event

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController
 
-	def create
-		User.create(user_params)
-		redirect_to events_path
+	def new
+		@user = User.new
 	end
 
-	private
-		def user_params
-			params.permit(:name, :email)
-		end
+	def create
+		# create a new user
+		@user = User.new( name: params[:user][:name], email: params[:user][:email], password: params[:user][:password], role: :user)
+
+		if @user.save
+				# Assign this user to the talk they wanted
+				new_attendee = Attendee.new(user_id: @user.id, event_id: params[:user][:event_id])
+				if new_attendee.save
+        	redirect_to root_path
+      	else
+      		redirect_to :back
+      	end
+      else
+        render :new
+      end
+	end
+
+
 end
