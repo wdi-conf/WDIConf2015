0856d3163420b46f3c4526723add609529cb91ec
Aden Margheriti <adennn@gmail.com>
2015-05-14 15:52:46 +1000

    added slideout nav and homepage styling

--- app/assets/javascripts/navBar.js
+++ app/assets/javascripts/navBar.js
@@ -1,13 +1,16 @@
-document.getElementById('navbar').addEventListener('mouseover', function(){
-    if (this.classList.contains('default')){
-      this.classList.remove('default');
-      this.classList.add('nav-expand');
-    }
-  });
+document.addEventListener("DOMContentLoaded", function() {
 
-document.getElementById('navbar').addEventListener('mouseout', function(){
-    if (this.classList.contains('nav-expand')){
-      this.classList.remove('nav-expand');
-      this.classList.add('default');
-    }
-  });
+  document.getElementById('navbar').addEventListener('mouseover', function(){
+      if (this.classList.contains('default')){
+        this.classList.remove('default');
+        this.classList.add('nav-expand');
+      }
+    });
+
+  document.getElementById('navbar').addEventListener('mouseout', function(){
+      if (this.classList.contains('nav-expand')){
+        this.classList.remove('nav-expand');
+        this.classList.add('default');
+      }
+    });
+});
