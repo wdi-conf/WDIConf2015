afbfe935c9bd0a7b26de8feec0a81ef889021004
Matt Cameron <mcameron@avalanche.com.au>
2015-05-20 15:44:22 +1000

    Add team images to about section

--- app/assets/javascripts/uiAnimations.js
+++ app/assets/javascripts/uiAnimations.js
@@ -3,27 +3,11 @@ var speaker = document.getElementsByClassName('speaker-listing');
 var bio = document.getElementsByClassName('bio');
 
 
-
-// $('.speaker-listing').hover(function() {
-//   bio = $(this).find('.bio')
-//   // $(bio).toggle("blind");
-//   // $('.speaker-listing').toggleClass('dimmed');
-//   // $(this).toggleClass('active');
-//   // $(bio).toggleClass('active');
-// });
-
-
   $('.speaker-listing').hover(function() {
     bio = $(this).find('.bio')
       $(bio).toggleClass("active");
       $(bio).toggleClass("hidden");
-      console.log('click');
   });
-});
-
-
-
-
 
 
 
