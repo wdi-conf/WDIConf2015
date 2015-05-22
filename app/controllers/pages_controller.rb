5e69d3c97fde90c9f39d41f4ad2146a933d2090d
Matt Cameron <mcameron@avalanche.com.au>
2015-05-22 11:20:58 +1000

    events right are events right

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 	def index
 		@events = Event.all
 		@events_left = Event.first(8)
-		@event_right = Event.last(8)
+		@events_right = Event.last(8)
 		@user = User.new
 	end
 
